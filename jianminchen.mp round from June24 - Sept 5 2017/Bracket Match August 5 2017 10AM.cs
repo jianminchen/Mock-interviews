6864bac02c3c23a08604f1272fba11729cc703e2
Jianmin Chen <jianminchen.fl@gmail.com>
2018-04-26 13:18:34 -0700

    check in

---
+++ jianminchen.mp round from June24 - Sept 5 2017/Bracket Match August 5 2017 10AM.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Solution
+{
+    public static int BracketMatch(string text) //((), ())(
+    {
+        // your code goes here
+        if (text == null || text.Length == 0) //
+        {
+            return 0;
+        }
+
+        int length = text.Length; // 3, 4
+        var openBracket = 0;
+        var misMatch = 0;
+
+        foreach (var item in text)
+        {
+            bool isOpen = item == '('; // 
+
+            if (isOpen)
+            {
+                openBracket++; // 1, 2, 1, 0, 1
+            }
+            else
+            {
+                if (openBracket > 0)
+                {
+                    openBracket--; // 1, 0
+                }
+                else
+                {
+                    misMatch++; // 1
+                }
+            }
+        }
+
+        return openBracket + misMatch; // 1,, 2 
+    }
+
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+// (() -> var open ( +1, (, open 2, ), open 1 , return 1
+// (()) -> open 0->1->2->1->0  , 0 
+// ())) -> open 0->1->0-> mismatch 1, 2 -> open + mistmatch 
+// linear scan O(n), space complexity -> O(1)
