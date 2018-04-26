50788b490d1e837261d23671d4e07827bde90dc5
Jianmin Chen <jianminchen.fl@gmail.com>
2018-04-26 10:54:33 -0700

    check in three mock interview source code

---
+++ jianminchen.sh Sept 7 - Nov 20 2017/Find the duplicates Sept 22 10PM 2017.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    public static int[] FindDuplicates(int[] arr1, int[] arr2)
+    {
+        // your code goes here
+        // 
+        if (arr1 == null || arr1.Length == 0 || arr2 == null || arr2.Length == 0)
+        {
+            return new int[0];
+        }
+
+        // not empty - arr1 - short one, arr2 - longer one
+        var length1 = arr1.Length;
+        var length2 = arr2.Length;
+
+        var firstOneSmall = length1 <= length2;
+        if (!firstOneSmall)
+        {
+            return FindDuplicates(arr2, arr1);
+        }
+
+        var list = new List<int>();
+
+        foreach (var number in arr1) // short one 
+        {
+            if (binarySearch(number, arr2))
+            {
+                list.Add(number);
+            }
+        }
+
+        return list.ToArray();
+    }
+
+    private static bool binarySearch(int search, int[] numbers)
+    {
+        var length = numbers.Length;
+
+        var start = 0;
+        var end = length - 1;
+
+        while (start <= end)
+        {
+            int middle = start + (end - start) / 2;
+
+            // assume that middle >= 0 => end > = start
+            var middleValue = numbers[middle];
+
+            var found = search == middleValue;
+            var less = search < middleValue;
+
+            if (found)
+            {
+                return true;
+            }
+
+
+            if (less)
+            {
+                end = middle - 1;
+            }
+            else
+            {
+                start = middle + 1;
+            }
+        }
+
+        return false;
+    }
+
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+// M is close to N, 
+// 1, 2, 3, 4, 6, 7
+// -> 2 -> 3 -> 
+// 3 6 7 8 20 
+// 3 -> 
+// while (index1 < length1 && index2 < length2)
+// O(max(m, n))  m + n
+// M >> N
+// go over each number in small size array, binary search in big array 
---
+++ jianminchen.sh Sept 7 - Nov 20 2017/Smallest Substring with All Characters Sept 25 10PM 2017.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    public static string GetShortestUniqueSubstring(char[] search, string source)
+    {
+        if (search == null || search.Length == 0)
+        {
+            throw new ArgumentException("Search collection is null or empty.", "Search argument"); // code review 1
+        }
+
+        // assume that search string is not empty
+        if (string.IsNullOrEmpty(source))
+        {
+            return "";
+        }
+
+        // put unique chars in search string to the dictionary
+        var map = search.ToDictionary(c => c, c => 1);  // code review 3
+
+        var needChars = search.Length; // 'xyz' - 3, var match = needChars == 0 
+        // iterate the string and find match, and also keep track of minimum 
+        var left = 0;
+        var length = source.Length;
+
+        var smallestLength = length + 1;
+        var smallestSubstring = "";
+
+        for (int index = 0; index < length; index++)
+        {
+            var visit = source[index];
+
+            //TryGetValue method to avoid double search of the value by key. 
+            int count;   // code review
+            var inMap = map.TryGetValue(visit, out count); // code review 4
+            var needOne = inMap && count > 0;
+
+            if (inMap)
+            {
+                map[visit]--;
+            }
+
+            if (needOne)
+            {
+                needChars--;
+            }
+
+            var findMatch = needChars == 0;
+            if (!findMatch)
+            {
+                continue;
+            }
+
+            // move left point forward - while loop                
+            while (left <= index && (!map.ContainsKey(source[left]) || (map[source[left]] < 0))) // code review 5
+            {
+                var removeChar = source[left];
+
+                // update the variable needChars                     
+                if (map.ContainsKey(source[left]))
+                {
+                    map[removeChar]++;
+                }
+
+                left++;
+            }
+
+            var currentLength = index - left + 1;
+            var findSmallerOne = currentLength < smallestLength;
+            if (findSmallerOne)
+            {
+                smallestLength = currentLength;
+                smallestSubstring = source.Substring(left, currentLength);
+
+                needChars++;
+                map[source[left]]++;
+                left = left + 1;
+            }
+        }
+
+        // edge case
+        return smallestLength == length + 1   // code review 6
+            ? string.Empty
+            : smallestSubstring;
+    }
+
+
+    static void Main(string[] args)
+    {
+
+    }
+}
+
