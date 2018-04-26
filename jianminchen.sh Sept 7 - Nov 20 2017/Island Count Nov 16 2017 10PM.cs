93afd803876644d94413be4b1d8cb80f5bc0e7a4
Jianmin Chen <jianminchen.fl@gmail.com>
2018-04-26 10:24:26 -0700

    check in those source code of mock interview

---
+++ jianminchen.sh Sept 7 - Nov 20 2017/Island Count Nov 16 2017 10PM.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    public static int GetNumberOfIslands(int[,] binaryMatrix)
+    {
+        if (binaryMatrix == null || binaryMatrix.GetLength(0) == 0) // false
+        {
+            return 0;
+        }
+
+        // base case
+        if (binaryMatrix.GetLength(0) == 1 && binaryMatrix.GetLength(1) == 1)
+        {
+            Console.WriteLine("code is here " + binaryMatrix[0, 0]);
+            return binaryMatrix[0, 0] == 1 ? 1 : 0;
+        }
+
+        var rows = binaryMatrix.GetLength(0); // 5, 5
+        var columns = binaryMatrix.GetLength(1);
+
+        var countIslands = 0;
+
+        // BFS visit all nodes on the island and then mark them as 0
+        var queue = new Queue<int[]>();
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < columns; col++)
+            {
+                var visit = binaryMatrix[row, col];
+                var isIsland = visit == 1;
+
+                if (!isIsland)
+                {
+                    continue;
+                }
+
+                countIslands++; // 1                    
+
+                binaryMatrix[row, col] = 0; // mark visit before pushing to queue
+                queue.Enqueue(new int[] { row, col }); // new int[]{0, 1}
+
+                while (queue.Count > 0) // 1 > 0 true
+                {
+                    var node = queue.Dequeue(); // int[]{0. 1}
+
+                    var currentRow = node[0]; // 0
+                    var currentCol = node[1]; // 1
+
+                    // four neighbors - check if it 1, then mark it 0, push to queue
+                    getNeighborsAndPushQueue(queue, binaryMatrix, currentRow, currentCol, rows, columns);
+                }
+            }
+        }
+
+        return countIslands;
+    }
+
+    private static void getNeighborsAndPushQueue(Queue<int[]> queue, int[,] binaryMatrix, int currentRow, int currentCol, int rows, int cols)
+    {
+        // left
+        if (currentCol >= 1 && binaryMatrix[currentRow, currentCol - 1] == 1) // 0, false
+        {
+            binaryMatrix[currentRow, currentCol - 1] = 0;
+            queue.Enqueue(new int[] { currentRow, currentCol - 1 });
+        }
+
+        // right
+        if (currentCol < (cols - 1) && binaryMatrix[currentRow, currentCol + 1] == 1) // false 
+        {
+            binaryMatrix[currentRow, currentCol + 1] = 0;
+            queue.Enqueue(new int[] { currentRow, currentCol + 1 });
+        }
+
+        // top 
+        if (currentRow > 0 && binaryMatrix[currentRow - 1, currentCol] == 1) // false 
+        {
+            binaryMatrix[currentRow - 1, currentCol] = 0;
+            queue.Enqueue(new int[] { currentRow - 1, currentCol });
+        }
+
+        // bottom
+        if (currentRow < rows - 1 && binaryMatrix[currentRow + 1, currentCol] == 1) // false
+        {
+            binaryMatrix[currentRow + 1, currentCol] = 0;
+            queue.Enqueue(new int[] { currentRow + 1, currentCol });
+        }
+    }
+
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+/*
+0 A 0  B 0  - 0 1 -> 2 -> 0 -> 0 -> count ++ 5 row * 5 column 
+0 0    0 0 0
+0 C    0 0 0 
+0 D 0 0 0
+E 0     0 0 F -> 6 
+
+more easy -> recursive depth of stack 100 * 100 = 10000 - stack size 
+*/
---
+++ jianminchen.sh Sept 7 - Nov 20 2017/Pancake sort Nov 15 2017 10PM .cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+class Solution
+{
+    public static int[] PancakeSort(int[] arr)
+    {
+        if (arr == null || arr.Length == 0)
+        {
+            return new int[0];
+        }
+
+        // let me flip 5 from test case [1, 5, 4, 3, 2] to the last one
+        var length = arr.Length;
+
+        var countFound = 0;
+
+
+        while (countFound < length)  // 5 , 1 < 5
+        {
+            var maxIndex = 0;
+            var maxValue = arr[0]; // [2, 3, 4, 1, 5]
+
+
+            var subArrayLength = length - countFound;  // 5, 4
+
+            // iterate the subarray 
+            for (int i = 1; i < subArrayLength; i++) // 
+            {
+                var visit = arr[i];
+                var isBigger = visit > maxValue;
+                if (isBigger)
+                {
+                    maxValue = visit;
+                    maxIndex = i;
+                }
+            }
+
+            flip(arr, maxIndex + 1); // 1
+            flip(arr, subArrayLength); // 5, 4
+
+            countFound++; // 1
+        }
+
+        return arr;
+    }
+
+    // reverse
+    private static void flip(int[] arr, int k)
+    {
+        if (k <= 0)
+        {
+            return;
+        }
+
+        int start = 0;
+        int end = k - 1;
+
+        while (start < end)
+        {
+            var tmp = arr[start];
+
+            arr[start] = arr[end];
+            arr[end] = tmp;
+
+            start++;
+            end--;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+/*
+arr = [1, 5, 4, 3, 2]
+flip(arr, k), first k element - reverse 
+[1, 2, 3, 4, 5]
+sort the array left -> right 
+right -> left 
+above 5 -> O(n) -> 1
+flip -> 5
+flip first 5 element -> last one 
+O(n) + O(n - 1) + ...+ O(1) = O(n*(n+1)/2 ) time complexity 
+extra array O(1)
+
+edge: null, 0
+size = 1
+sample test case 
+
+*/
