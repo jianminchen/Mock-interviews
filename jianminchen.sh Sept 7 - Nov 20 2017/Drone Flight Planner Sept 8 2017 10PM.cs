56c58f9e5a0f34c699ad4d5cbb81ac5fcb3c3307
Jianmin Chen <jianminchen.fl@gmail.com>
2018-04-26 11:04:48 -0700

    check in

---
+++ jianminchen.sh Sept 7 - Nov 20 2017/Drone Flight Planner Sept 8 2017 10PM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Solution
+{
+    public static int CalcDroneMinEnergy(int[,] route)
+    {
+        // your code goes here
+        if (route == null || route.GetLength(0) == 0 || route.GetLength(1) == 0)
+        {
+            return 0;
+        }
+
+        //assume the array is not empty; 
+        var rows = route.GetLength(0); // 5, 
+        //var columns = route.GetLength(1); // 3 third element - height, ignore first and second one
+
+        var maxHeight = route[0, 2]; // 10
+
+        for (int row = 1; row < rows; row++) // 1 - 4
+        {
+            var visitHeight = route[row, 2]; // 0, 6, 15, 8 
+
+            maxHeight = visitHeight > maxHeight ? visitHeight : maxHeight; // 15
+        }
+
+        return maxHeight - route[0, 2];  // 0 -> 15 - 10 
+    }
+
+    static void Main(string[] args)
+    {
+        var initialEnergy = CalcDroneMinEnergy(new int[,] { { 0, 2, 10 }, { 3, 5, 0 }, { 9, 20, 6 }, { 10, 12, 15 }, { 10, 10, 8 } });
+        Console.WriteLine(initialEnergy);
+    }
+}
+
+// 15                                             -- 0Kwh
+//
+//
+//
+//
+// 10   - 5wh                             -- 0KWh -> 5kwh
+// 9
+//                                                                -- 7Kwh  -- 
+//
+// 6                         --  10 - 6 = 4kWh
+//
+//
+//
+//
+//
+// 0             -- 10KWh
+
+// [[0, 0, 0], [1, 1, 2], [1, 2, 1]]
+// answer = 2
