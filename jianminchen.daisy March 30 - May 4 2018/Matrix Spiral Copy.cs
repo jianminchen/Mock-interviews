18b944db4642eb04ce8f2304a3d6d223d9d20dbc
Jianmin Chen <jianminchen.fl@gmail.com>
2018-06-15 16:50:32 -0700

    check in source code

---
+++ jianminchen.daisy March 30 - May 4 2018/Matrix Spiral Copy.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    public static int[] SpiralCopy(int[,] inputMatrix)
+    {
+      if(inputMatrix == null)
+        return new int[0]; 
+      
+      var rows    = inputMatrix.GetLength(0); 
+      var columns = inputMatrix.GetLength(1); 
+      
+      var totalNumber = rows * columns; 
+      var directions = new List<int[]>(); 
+      directions.Add(new int[]{0, 1}); // go right
+      directions.Add(new int[]{1, 0}); // go down
+      directions.Add(new int[]{0, -1}); // go left
+      directions.Add(new int[]{-1, 0}); // go up
+      
+      var visited = new bool[rows, columns]; // false 
+      
+      var direction = 0; 
+      
+      // 0, -1 -> 
+      int row = 0; 
+      int col = -1; 
+      int index = 0; 
+      
+      var spiral = new int[totalNumber]; 
+      
+      while(index < totalNumber)
+      {
+          // go right -> 1, 2, 3, 4, 5
+          var nextRow = row + directions[direction][0];  // 0 1 + 0
+          var nextCol = col + directions[direction][1];  // 0 0 + 1
+        
+          if(nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < columns && !visited[nextRow, nextCol])// 
+          {
+            spiral[index++] = inputMatrix[nextRow, nextCol];  
+            
+            visited[nextRow, nextCol] = true; 
+            
+            row = nextRow; 
+            col = nextCol; 
+          }
+          else 
+          {
+            direction = (direction + 1) % 4; // 0 
+          }
+      }
+      
+      return spiral;       
+    }
+
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+/*
+1 2 3 4 5 -> automatically -> visited 1 2 3 4 5 -> change direction -> 
+  right: [0, 1] 
+  down:  [1, 0]
+  left:  [0, -1]
+  up: [-1, 0]
+  */
+/* the peer shared his code:
+ * 
+ if (matrix.GetLength(0) == 0 || matrix.GetLength(1) == 0)
+            return new int[]{};
+        
+        int r1 = 0, r2 = matrix.GetLength(0) - 1;
+        int c1 = 0, c2 = matrix.GetLength(1) - 1;
+        int[] ans = new int[(r2 + 1) * (c2+1)];
+        int k =0;
+        while (r1 <= r2 && c1 <= c2) 
+        {
+            for (int c = c1; c <= c2; c++) 
+                ans[k++] = matrix[r1, c];
+            
+            for (int r = r1 + 1; r <= r2; r++) 
+                ans[k++] = matrix[r, c2];
+            
+            if (r1 < r2 && c1 < c2) 
+            {
+                for (int c = c2 - 1; c > c1; c--) 
+                    ans[k++] = matrix[r2, c];
+                
+                for (int r = r2; r > r1; r--) 
+                    ans[k++] = matrix[r, c1];
+            }
+            
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+  */    
---
+++ jianminchen.daisy March 30 - May 4 2018/Word Count Engine.cs
@@ -0,0 +1,136 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+class Solution
+{
+    public static string[,] WordCountEngine(string document)
+    {
+        if (document == null)
+            return new string[0, 0];
+
+        var lowerCase = document.ToLower();
+
+        // you'll -> remove ' 
+        var removed = removeSpecialChar(lowerCase);
+
+        var split = removed.Split(" !.;:,?".ToCharArray());
+
+        var dict = saveToDictionary(split);
+
+        var totalCount = 0;
+        var buckets = applyBucketSortKeepTheOriginalOrder(dict, split, ref totalCount);
+        return convertToDimension(buckets, totalCount);
+    }
+
+    private static string removeSpecialChar(string lowerCase)
+    {
+        // '''
+        var sb = new StringBuilder();
+        foreach (var item in lowerCase)
+        {
+            if (item != '\'')
+            {
+                sb.Append(item);
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    private static Dictionary<string, int> saveToDictionary(string[] split)
+    {
+        var dict = new Dictionary<string, int>();
+
+        foreach (string item in split)
+        {
+            if (item == null || item.Length == 0)
+                continue;
+
+            if (dict.ContainsKey(item))
+            {
+                dict[item]++;
+            }
+            else
+            {
+                dict.Add(item, 1);
+            }
+        }
+
+        return dict;
+    }
+
+    private static string[,] convertToDimension(IList<string>[] buckets, int totalCount)
+    {
+        var length = buckets.Length;
+
+        var sorted = new string[totalCount, 2];
+
+        int index = 0;
+
+        for (int bucketIndex = length - 1; bucketIndex > 0; bucketIndex--)
+        {
+            // buckets[index] -> this is a list         
+            foreach (var item in buckets[bucketIndex])
+            {
+                sorted[index, 0] = item;
+                sorted[index, 1] = bucketIndex.ToString();
+                index++;
+            }
+        }
+
+        return sorted;
+    }
+
+    private static IList<string>[] applyBucketSortKeepTheOriginalOrder(Dictionary<string, int> dict, string[] split, ref int totalCount)
+    {
+        int maxValue = dict.Values.Max();
+        var buckets = new List<string>[maxValue + 1];  // 0 -> maxValue 
+
+        for (int index = 0; index < maxValue + 1; index++)
+        {
+            buckets[index] = new List<string>();
+        }
+
+        var hashSet = new HashSet<string>();
+
+        foreach (var word in split)
+        {
+            if (hashSet.Contains(word) || word.Length == 0)
+                continue;
+
+            hashSet.Add(word);
+
+            var value = dict[word];
+
+            buckets[value].Add(word);
+
+            totalCount++;
+        }
+
+        return buckets;
+    }
+
+    static void Main(string[] args)
+    {
+        var sorted = WordCountEngine("a b c c d");
+        for (int row = 0; row < sorted.GetLength(0); row++)
+        {
+            Console.WriteLine(sorted[row, 0] + " " + sorted[row, 1]);
+        }
+    }
+}
+
+/*
+requirement:
+
+convert to lower case
+split words by delimiters - " .,!"
+put to dictionary - key - string - value 3 -> Max value 
+bucket sort -> 
+keep the order in the original sentence -> go through the split words -> put into bucket 
+output to dimension array -> 
+
+*/
