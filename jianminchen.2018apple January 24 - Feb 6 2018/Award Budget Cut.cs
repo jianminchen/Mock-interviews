437e8cbb0daabe3797816c9936e4f601c634166c
Jianmin Chen <jianminchen.fl@gmail.com>
2018-06-18 11:49:19 -0700

    check in 2018apple around source code

---
+++ jianminchen.2018apple January 24 - Feb 6 2018/Award Budget Cut.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Solution
+{
+    public static double FindGrantsCap(double[] grantsArray, double newBudget)
+    {
+        if (grantsArray == null || grantsArray.Length == 0) // false
+        {
+            return 0;
+        }
+
+        Array.Sort(grantsArray); // 2, 50, 100, 120, 1000
+
+        double sumOfSeries = 0; // 0
+
+        var length = grantsArray.Length;  // 5
+
+        for (int index = 0; index < length; index++)
+        {
+            // do a survive test 
+            double current = grantsArray[index]; // 2, 50
+            int restNumbers = (length - index);// 5, 4
+
+            var canSurvive = sumOfSeries + current * restNumbers <= newBudget; // true, false
+            if (!canSurvive)
+            {
+                return (newBudget - sumOfSeries) / restNumbers; // 47
+            }
+
+            sumOfSeries += current; // 2
+        }
+
+        return grantsArray[length - 1];
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(FindGrantsCap(new double[] { 2, 100, 50, 120, 1000 }, 190));
+    }
+}
+/*
+keywords:
+
+given an array, new budget, double 
+ask cap value, 
+constraint: the least number of recipents is impacted 
+
+nlogn -
+2, 50, 100, 120, 1000, new budget 190
+2, 47, 47 , 47, 47  - impacted 4 of them 
+
+-> scan the array, see if the current value can be survived. 
+  2 -> 2 * 5 < 190 - true
+  dynamic programming 2 + 
+  2 + 50 * 4 < 190 - false 
+  2 + cap * 4 = 190 -> cap = 188/ 4 = 47 
+  
+  
+  */
---
+++ jianminchen.2018apple January 24 - Feb 6 2018/Merge 2 Packages.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    public static int[] GetIndicesOfItemWeights(int[] arr, int limit) // [4, 6, 10,1 5, 16], lim = 21
+    {
+        if (arr == null || arr.Length < 2 || limit < 0) // false
+        {
+            return new int[0];
+        }
+
+        var dict = new Dictionary<int, int>();
+        var length = arr.Length; // 6
+
+        for (int index = 0; index < length; index++)
+        {
+            var current = arr[index]; // 4, 6, 10, 15
+            var search = limit - current; // 17, 15, 11, 6
+
+            if (dict.ContainsKey(search)) // false, true 
+            {
+                var value = dict[search]; // 1
+                return new int[] { Math.Max(value, index), Math.Min(value, index) };
+            }
+            else
+            {
+                dict[current] = index; // [4, 0], [6, 1], [10, 2]
+            }
+        }
+
+        return new int[0];
+    }
+
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+/*
+keywords: 
+given limit - limit
+  
+  integer array - not sorted 
+  
+search two indexes [i, j], i > j, arr[i] + arr[j] = limit
+
+Find one pair only 
+
+scan the array from left to array, 
+
+[4, 6, 10, 15, 16]  limit = 21
+  
+  dictionary<int, int> 
+  4 -> [4, 0]
+  6 -> 15 in dict -> [6, 1]
+  10 ->11 in dict -> [10, 2] 
+  15 -> 6 in dict ? [6, 1], [3, 1] 
+  
+  O(1) -> hashset, dictionary 
+  look up by value, find its index -> 
+  
+  Time space: O(n) - 
+
+*/
