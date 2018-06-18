44f57c45e26b3fec4055cbcfb0a006c89bbfc85a
Jianmin Chen <jianminchen.fl@gmail.com>
2018-06-18 12:07:29 -0700

    check in three more practice

---
+++ jianminchen.2018apple January 24 - Feb 6 2018/Bracket Match.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Solution
+{
+    public static int BracketMatch(string text)  // "(()"
+    {
+
+        if (text == null || text.Length == 0) // false
+        {
+            return 0;
+        }
+
+        var length = text.Length; // 3
+        int openBracket = 0;
+        int unmatchedCloseBracket = 0;
+
+        for (int i = 0; i < length; i++)
+        {
+            var visit = text[i]; // ( (  )
+            var isOpen = visit == '(';
+            if (isOpen)
+            {
+                openBracket++; // 1 -> 2
+            }
+            else
+            {
+                if (openBracket > 0)
+                {
+                    openBracket--; // 1
+                }
+                else
+                    unmatchedCloseBracket++;
+            }
+        }
+
+        return openBracket + unmatchedCloseBracket; // 1
+
+    }
+
+    static void Main(string[] args)
+    {
+
+    }
+}
+
