9ce76b1465b1593f48cbd645d2fe670b18de9a57
Jianmin Chen <jianminchen.fl@gmail.com>
2018-06-25 10:01:15 -0700

    check in the source code

---
+++ jianminchen.erica June 16 -/BST Successor Search.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Solution
+{
+    public class Node
+    {
+        public int value;
+        public Node left;
+        public Node right;
+        public Node parent;
+    }
+
+    public static Node findInOrderSuccessor(Node inputNode)
+    {
+        if (inputNode == null)
+            return null;
+
+        if (inputNode.right != null)
+        {
+            // find leftmost  node in right subtree
+            return findLeftMostNode(inputNode.right);
+        }
+        else
+        {
+            var iterate = inputNode;
+            while (iterate.parent != null && iterate.parent < inputNode.value)
+            {
+                iterate = iterate.parent;
+            }
+
+            return iterate.parent;
+        }
+    }
+
+    private static Node findLeftMostNode(Node root)
+    {
+        var iterate = root;
+        while (iterate.left != null)
+            iterate = iterate.left;
+
+        return iterate;
+    }
+
+    static void Main(string[] args)
+    {
+        // test findInOrderSuccessor here
+    }
+}
+
+// keywords: BST, inorder successor, 
+// given a node inputNode in a BST, asking: inorder successor
+// in other words, bigger, smallest one in all bigger elements
+
+// brute force, inorder traversal -> 5, 9, 11, 12, 14, 20, 25, given node 9, search the array to find 9, next element - O(n)
+// try to do O(logn), try to start from given node, back to root node or down to leaf node, try to find successor
