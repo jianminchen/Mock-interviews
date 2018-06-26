86f7842e542df547709cb230c4c34491f45ab854
Jianmin Chen <jianminchen.fl@gmail.com>
2018-06-26 15:44:32 -0700

    check in deletion distance algorithm practices

---
+++ Sort by Algorithm/Deletion distance/003 Deletion Distance round sh.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+class Solution
+{
+    public static int DeletionDistance(string str1, string str2)
+    {
+        // your code goes here
+        if ((str1 == null || str1.Length == 0) && (str2 == null || str2.Length == 0))
+        {
+            return 0;
+        }
+
+        // assume that one of is not empty
+        if (str1 == null || str1.Length == 0)
+        {
+            return str2.Length;
+        }
+
+        if (str2 == null || str2.Length == 0)
+        {
+            return str1.Length;
+        }
+
+        var length1 = str1.Length;
+        var length2 = str2.Length;
+
+        var memo = new int[length1][];
+        for (int row = 0; row < length1; row++)
+        {
+            memo[row] = new int[length2];
+
+            for (int col = 0; col < length2; col++)
+            {
+                memo[row][col] = -1; // not set
+            }
+        }
+
+        return DeletionDistanceHelper(str1, str2, 0, 0, memo);
+    }
+
+    private static int DeletionDistanceHelper(string str1, string str2, int index1, int index2, int[][] memo)
+    {
+        var length1 = str1.Length;
+        var length2 = str2.Length;
+
+        if (index1 == length1)
+        {
+            return length2 - index2;  // outside -> 
+        }
+
+        if (index2 == length2)
+        {
+            return length1 - index1;
+        }
+
+        var hasMemo = memo[index1][index2] != -1;
+
+        if (hasMemo)
+        {
+            return memo[index1][index2];
+        }
+
+        var visit1 = str1[index1];
+        var visit2 = str2[index2];
+
+        var isEqual = visit1 == visit2;
+
+        // check if need to update memo
+        var distance = 0;
+
+        if (isEqual)
+        {
+            distance = DeletionDistanceHelper(str1, str2, index1 + 1, index2 + 1, memo);
+        }
+        else
+        {
+            // delete one of chars in two strings 
+            distance = 1 + Math.Min(DeletionDistanceHelper(str1, str2, index1 + 1, index2, memo),
+                                    DeletionDistanceHelper(str1, str2, index1, index2 + 1, memo));
+        }
+
+        memo[index1][index2] = distance; // do not calculate twice 
+
+        return distance;
+    }
+
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+// dynamic program -> recursive -> memo 
+// heat -> hit distance 3
+// 'h' == 'h', 
+// index1 for heat -> distance same, index1 ++
+// index2 for hit                    index2 ++
+// 'e' ?= 'i', delete 'e' or delete 'i'
+// Min(dist("at", "it"), dist("eat", "t")) + 1, count deletion, increment one 
+// memo   use jagged array to express memo 
+// "heat" - 4
+// "hit"   - 3
+// memo[4][3], 
+// memo[row][0] = row, for any row from 0 to 3
+// memo[0][col] = col, for any column for o to 2
+// recursive, memoization 
+// n * m , n is length of str1, m is length of str2 - wrong 
+// 2^max(n, m) - without memoization -> 
+// with memoization, n * m 
+
+
---
+++ Sort by Algorithm/Deletion distance/008 Deletion Distance round beet.cs
@@ -0,0 +1,85 @@
+﻿
+using System;
+
+class Solution
+{
+    public static int DeletionDistance(string str1, string str2)
+    {
+      if(str1 == null || str2 == null)
+      {
+        return 0;
+      }
+      
+      var length1 = str1.Length; 
+      var length2 = str2.Length; 
+      
+      var dp = new int[length1 + 1, length2 + 1]; 
+      
+      // base case
+      for(int col = 0; col < length2 + 1; col++)
+      {
+        dp[0, col] = col; 
+      }
+      
+      for(int row = 0; row < length1 + 1; row++)
+      {
+        dp[row, 0] = row; 
+      }
+      
+      // inductive step 
+      for(int row = 1; row < length1 + 1; row++)
+      {
+        for(int col = 1; col < length2 + 1; col++)
+        {
+          var char1 = str1[row - 1]; 
+          var char2 = str2[col - 1]; 
+          var isEqual = char1 == char2; 
+          
+          if(isEqual)
+          {
+            dp[row, col] = dp[row - 1, col - 1];
+          }
+          else 
+          {
+            dp[row, col] = 1 + Math.Min(dp[row - 1, col], dp[row, col - 1]); 
+          }
+        }
+      }
+      
+      return dp[length1, length2]; 
+    }
+
+    static void Main(string[] args)
+    {
+
+    }
+}
+
+// time complexity  O((length1)(length2 ))  
+// brute force: 2^(length1 + length2)
+// space complexity : O(n * m)
+// optimal space complexity: O(min(m , n)))
+
+/*
+hit and heat how to calculate distance, using dynamic programing 
+
+        0     1     2      3        4
+        ""   "h"  "he"   "hea"   "heat"
+        ---------------------------------------------
+0  ""   0     1     2      3      4 
+1 "h"   1     0     1
+2  "hi" 2
+3 "hit" 3                          ? 
+  
+  
+  "h", "he"
+  
+   |     |    two choices: delete either one of latest char in two strings
+  
+              delete "h", 1 + dist("", "he")  = 1 + dist(0, 2) = 1 + 2
+              delete "e",  1 + dist("h", "h") = 1 + dist(1, 1) = 1 + 0 
+              Min(3, 1) = 1
+              
+               if current two chars are equal, then dist(i - 1, j - 1)
+               
+               */
